05a720f5273bf2b9bf2144418498a57b0cd04f2b
tognut86 <tognut86@gmail.com>
2022-05-19 19:44:11 +0200

    contentful sdk

---
+++ src/contentful/client.js
@@ -0,0 +1,8 @@
+import { createClient } from "contentful";
+
+const client = createClient ({
+    space: "xk2cxjb1kbgr", 
+    accessToken: "UbpGxxF5m9HdCZIUPwpgmcDRrrXKmsPaqcAeovxsI0g"
+});
+
+export default client;
